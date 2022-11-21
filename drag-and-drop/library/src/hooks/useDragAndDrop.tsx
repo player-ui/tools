14ddc3279d9f4749e32810c4bb1374e51a81a018
Ketan Reddy <ketan_reddy@intuit.com>
2022-11-21 15:36:51 -0800

    Use XLR

--- drag-and-drop/library/src/hooks/useDragAndDrop.tsx
+++ drag-and-drop/library/src/hooks/useDragAndDrop.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { DragAndDropControllerOptions } from '../controller';
 import { DragAndDropController } from '../controller';
 
-export const useDragAndDrop = (conf?: DragAndDropControllerOptions) => {
+export const useDragAndDrop = (conf: DragAndDropControllerOptions) => {
   const controller = React.useMemo(() => {
     return new DragAndDropController(conf);
   }, []);
