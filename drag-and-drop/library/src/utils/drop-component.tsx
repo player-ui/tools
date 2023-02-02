bb6b2d4d355629ca67b8c4fe593e86838cda5394
Ketan Reddy <ketan_reddy@intuit.com>
2023-02-01 17:01:55 -0800

    Support for Arrays and Collections

--- drag-and-drop/library/src/utils/drop-component.tsx
+++ drag-and-drop/library/src/utils/drop-component.tsx
@@ -3,8 +3,27 @@ import { ReactAsset } from '@player-ui/react';
 import type { TransformedDropTargetAssetType } from '../types';
 import { useDroppableAsset } from '../hooks/useDroppableAsset';
 
+export const DropPrependComponent = (
+  props: TransformedDropTargetAssetType & {
+    action: 'prepend' | 'append';
+  }
+) => {
+  const { action } = props;
+  const [{ isOver }, drop] = useDroppableAsset(props, action);
+
+  return (
+    <div ref={drop} style={{ border: isOver ? '1px solid red' : undefined }}>
+      <span>
+        {action} - {props.context?.parent.assetName} -{' '}
+        {props.context?.propertyName}
+      </span>
+    </div>
+  );
+};
+
 export const DropComponent = (props: TransformedDropTargetAssetType) => {
-  const [{ isOver }, drop] = useDroppableAsset(props);
+  const [{ isOver }, drop] = useDroppableAsset(props, 'replace');
+  const isArrayInsertion = props.context?.isArrayElement ?? false;
 
   if (!props.value && !props.context) {
     return (
@@ -16,11 +35,24 @@ export const DropComponent = (props: TransformedDropTargetAssetType) => {
 
   return (
     <div ref={drop} style={{ border: isOver ? '1px solid red' : undefined }}>
-      {props.value ? (
-        <ReactAsset {...props.value.asset} />
-      ) : (
+      {props.value && (
+        <>
+          {isOver && <DropPrependComponent {...props} action="prepend" />}
+          <ReactAsset {...props.value.asset} />
+          {isOver && <DropPrependComponent {...props} action="append" />}
+        </>
+      )}
+
+      {!props.value && !isArrayInsertion && (
         <span>
-          {props.context?.parent.name} - {props.context?.propertyName}
+          {props.context?.parent.assetName} - {props.context?.propertyName}
+        </span>
+      )}
+
+      {!props.value && isArrayInsertion && (
+        <span>
+          Insert into {props.context?.parent.assetName} -{' '}
+          {props.context?.propertyName} List
         </span>
       )}
     </div>
