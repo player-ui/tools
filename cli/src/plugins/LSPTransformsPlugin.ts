ccc4d49ee553d9fb9b0863efffb19a1f8c6d722d
Ketan Reddy <ketan_reddy@intuit.com>
2024-02-06 20:23:07 -0800

    Actually export new LSP plugins

--- cli/src/plugins/LSPTransformsPlugin.ts
+++ cli/src/plugins/LSPTransformsPlugin.ts
@@ -2,18 +2,10 @@ import type { PlayerLanguageService } from '@player-tools/json-language-service'
 import type { TransformFunction } from '@player-tools/xlr';
 import type { PlayerCLIPlugin } from './index';
 
-export interface Config {
-  /** the path to the asset library to load */
-  path: string;
-
-  /** Provides experimental language features */
-  exp?: boolean;
-}
-
 /**
  * Handles adding XLR transforms to the LSP's XLR SDK
  */
-export class LSPAssetsPlugin implements PlayerCLIPlugin {
+export class LSPTransformsPlugin implements PlayerCLIPlugin {
   private functionsToLoad: Record<string, TransformFunction>;
 
   constructor(functionsToLoad: Record<string, TransformFunction>) {
