5fdbb9ea47ba257bcf2a58b25f7fb249dda0bedc
Ketan Reddy <ketan@ketanreddy.com>
2024-09-27 14:18:09 -0700

    Add options to LSPAssetsPlugin to load from TSManifest via module import

--- cli/src/plugins/LSPAssetsPlugin.ts
+++ cli/src/plugins/LSPAssetsPlugin.ts
@@ -1,13 +1,41 @@
 import type { PlayerLanguageService } from "@player-tools/json-language-service";
 import type { PlayerCLIPlugin } from "./index";
+import { TSManifest } from "@player-tools/xlr";
 
-export interface LSPAssetsPluginConfig {
-  /** the path to the asset library to load */
+/** Loads XLRs into the LSP via the manifest.js file */
+export interface LSPAssetsPluginModuleConfig {
+  type: "module";
+
+  /** Result of module import/require of xlr manifest.js file */
+  manifest: TSManifest;
+}
+
+/** Loads XLRs to the LSP by specifying a filesystem location */
+export interface LSPAssetsPluginManifestConfig {
+  type: "manifest";
+
+  /** Path to dist folder for XLR enabled plugins */
   path: string;
+}
 
+/**
+ * Legacy type for providing XLR manifest paths that assumes its a manifest
+ * @deprecated
+ * */
+export interface LSPAssetsPluginLegacyConfig {
+  type?: undefined;
+  path: string;
+}
+
+export type LSPAssetPluginConfigTypes =
+  | LSPAssetsPluginModuleConfig
+  | LSPAssetsPluginManifestConfig
+  | LSPAssetsPluginLegacyConfig;
+
+export type LSPAssetsPluginConfig = LSPAssetPluginConfigTypes & {
   /** Provides experimental language features */
   exp?: boolean;
-}
+};
 
 /**
  * Handles setting the assets when loading the LSP
@@ -36,9 +64,26 @@ export class LSPAssetsPlugin implements PlayerCLIPlugin {
     exp: boolean
   ): Promise<void> {
     if (Array.isArray(this.config)) {
-      await lsp.setAssetTypes(this.config.map((c) => c.path));
+      await Promise.all(
+        this.config.map((c) => {
+          this.loadConfig(c, lsp);
+        })
+      );
     } else {
-      await lsp.setAssetTypes([this.config.path]);
+      await this.loadConfig(this.config, lsp);
+    }
+  }
+
+  async loadConfig(
+    config: LSPAssetPluginConfigTypes,
+    lsp: PlayerLanguageService
+  ): Promise<void> {
+    if (config.type === "manifest" || config.type === undefined) {
+      await lsp.setAssetTypes([config.path]);
+    } else if (config.type === "module") {
+      await lsp.setAssetTypesFromModule([config.manifest]);
+    } else {
+      throw Error(`Unknown config type: ${(config as any).type}`);
     }
   }
 }
